366c3a395c680432114c1245f9ce6cdfa97dc7fc
Matias Mirko <mmffalkiewicz@gmail.com>
2019-05-06 22:01:41 -0300

    uncanal

--- src/api/Services/myml/myml_services.go
+++ src/api/Services/myml/myml_services.go
@@ -76,26 +76,34 @@ func GetRespuestaFromApiReceiver(userID int64) (*myml.JsonSuma, *apierrors.ApiEr
 
 	var respuesta myml.JsonSuma
 	var wg sync.WaitGroup
+	cE:=make(chan *apierrors.ApiError)
 
 	user := &myml.User{ID: int(userID)}
 	err := user.Get()
-	respuesta.User=*user
+	respuesta.User = *user
 	if err != nil {
 		return nil, &apierrors.ApiError{
 			Message: err.Message,
 			Status:  http.StatusInternalServerError,
 		}
 	}
-	wg.Add(2)
+
+	wg.Add(1)
+
+	go func() {
+		err = <-cE
+		wg.Done()
+	}()
 
 	go func() {
 		err = respuesta.Site.Get(user.SiteID)
-		wg.Done()
+		cE <- err
 	}()
 	go func() {
 		err = respuesta.Category.Get(user.SiteID)
-		wg.Done()
+		cE <- err
 	}()
+
 	wg.Wait()
 
 	if err != nil {
@@ -104,6 +112,7 @@ func GetRespuestaFromApiReceiver(userID int64) (*myml.JsonSuma, *apierrors.ApiEr
 			Status:  http.StatusInternalServerError,
 		}
 	}
+
 
 	return &respuesta, nil
 }
